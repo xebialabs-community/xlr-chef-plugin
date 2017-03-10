4e44a2754a12d9f7ab2e59ac959b4b4f4d15df30
Erik Rasmussen <erasmussen@xebialabs.com>
2017-03-10 13:49:29 -0700

    Windows Implementation
    Added necessary code to run plugin correctly if XL Release is running on
    a Windows machine.

--- src/main/resources/chef/Workstation.py
+++ src/main/resources/chef/Workstation.py
@@ -17,10 +17,7 @@ class Workstation(object):
         self.cmd_line = CmdLine()
         self.chef_sdk_path = workstation_config['chef_sdk_path']
         self.client_key  = Workstation.format_key(workstation_config['client_key'])
-        if workstation_config['os'] :
-            self.os = "UNIX"
-        else:
-            self.os = "WINDOWS"
+        self.unix = workstation_config['os']
         self.chef_server_url = workstation_config['chef_server_url']
         self.node_name = workstation_config['node_name']
         self.log_level = workstation_config['log_level']
@@ -43,16 +40,6 @@ class Workstation(object):
             chef_key = "%s%s\n" % (chef_key, l)
         chef_key = "%s%s\n" % (chef_key, footer)
         return chef_key
-
-    @staticmethod
-    def zip_workspace(workspace_path, connection):
-        zip_script = "#!/bin/bash\ncd %s\ntar -czf /tmp/chef.tgz ." % workspace_path
-        zip_script_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(workspace_path), 'zip.script'))
-        OverthereUtils.write(String(zip_script).getBytes(), zip_script_file)
-        zip_script_file.setExecutable(True)
-        command = CmdLine()
-        command.addArgument(zip_script_file.getPath())
-        return connection.execute(command)
 
     @staticmethod
     def process_flag_with_arguments(arguments, flag):
@@ -84,25 +71,25 @@ class Workstation(object):
         run_list_param = Workstation.process_flag_with_arguments(variables['run_list'], "--run-list")
         authentication_option = Workstation.process_unix_authentication(variables)
         return self.execute_knife_command("bootstrap %s --ssh-user %s %s %s %s --node-name %s %s --yes" \
-            % (variables['address'], variables['ssh_user'], authentication_option, sudo_param, sudo_password_param, variables['node_name'], run_list_param), 'chef_bootstrapunix.script', variables['options'], True)
+            % (variables['address'], variables['ssh_user'], authentication_option, sudo_param, sudo_password_param, variables['node_name'], run_list_param), 'chef_bootstrapunix.cmd', variables['options'], True)
 
     def chef_bootstrapwindows(self, variables):
         run_list_param = Workstation.process_flag_with_arguments(variables['run_list'], "--run-list")
         return self.execute_knife_command("bootstrap windows winrm %s --winrm-user %s --winrm-password '%s' --node-name %s %s --yes"
             % (variables['address'], variables['username'], variables['password'], variables['node_name'], run_list_param),
-            'chef_bootstrapwindows.script', variables['options'], True)
+            'chef_bootstrapwindows.cmd', variables['options'], True)
 
     def chef_cookbooklist(self, variables):
-        return self.execute_knife_command("cookbook list", 'chef_cookbooklist.script', variables['options'], True)
+        return self.execute_knife_command("cookbook list", 'chef_cookbooklist.cmd', variables['options'], True)
 
     def chef_nodelist(self, variables):
-        return self.execute_knife_command("node list", 'chef_nodelist.script', variables['options'], True)
+        return self.execute_knife_command("node list", 'chef_nodelist.cmd', variables['options'], True)
 
     def chef_clientlist(self, variables):
-        return self.execute_knife_command("client list", 'chef_clientlist.script', variables['options'], True)
+        return self.execute_knife_command("client list", 'chef_clientlist.cmd', variables['options'], True)
 
     def chef_deleteoperation(self, target, variables):
-        return self.execute_knife_command("%s delete %s --yes" % (target, variables['node_name']), 'chef_delete.script', variables['options'], True)
+        return self.execute_knife_command("%s delete %s --yes" % (target, variables['node_name']), 'chef_delete.cmd', variables['options'], True)
 
     def chef_deletenode(self, variables):
         return self.chef_deleteoperation("node", variables)
@@ -119,7 +106,7 @@ class Workstation(object):
             os_chef_client_command = "'chef-client'"
         command ="%s --yes 'name:%s' %s --yes --%s-user %s --%s-password '%s'" \
             % (os_specific_command, variables['node_name'], os_chef_client_command, os_specific_command, variables['username'], os_specific_command, variables['password'])
-        return self.execute_knife_command(command, 'chef_applycookbook.script', variables['options'], True)
+        return self.execute_knife_command(command, 'chef_applycookbook.cmd', variables['options'], True)
 
     def chef_applycookbookunix(self, variables):
         return self.chef_applycookbook(variables, True)
@@ -128,10 +115,10 @@ class Workstation(object):
         return self.chef_applycookbook(variables, False)
 
     def chef_setrunlist(self, variables):
-        return self.execute_knife_command("node run_list set %s 'recipe[%s]'" % (variables['node_name'], variables['run_list']), 'chef_setrunlist.script', variables['options'], True)
+        return self.execute_knife_command("node run_list set %s 'recipe[%s]'" % (variables['node_name'], variables['run_list']), 'chef_setrunlist.cmd', variables['options'], True)
 
     def chef_shownode(self, variables):
-        return self.execute_knife_command("node show %s" % variables['node_name'], 'chef_shownode.script', variables['options'], True)
+        return self.execute_knife_command("node show %s" % variables['node_name'], 'chef_shownode.cmd', variables['options'], True)
 
 
     def execute_knife_command(self, command, script_name, options=None, zip_workspace=False):
@@ -140,17 +127,17 @@ class Workstation(object):
             options = Workstation.process_additional_options(options)
             connection = LocalConnection.getLocalConnection()
             workspace_path = self.create_chef_tmp_workspace(connection)
-            knife_command = "#!/bin/bash\ncd %s\n%s/bin/knife %s %s" % (workspace_path, self.chef_sdk_path, command, options)
+            knife_command = self.get_os_specific_knife_command(workspace_path, command, options)
             script_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(workspace_path), script_name))
             OverthereUtils.write(String(knife_command).getBytes(), script_file)
             script_file.setExecutable(True)
+            if zip_workspace:
+                self.zip_workspace(workspace_path, connection)
             command = CmdLine()
             command.addArgument(script_file.getPath())
             output_handler = CapturingOverthereExecutionOutputHandler.capturingHandler()
             error_handler = CapturingOverthereExecutionOutputHandler.capturingHandler()
             exit_code = connection.execute(output_handler, error_handler, command)
-            if zip_workspace:
-                self.zip_workspace(workspace_path, connection)
             return [exit_code, output_handler]
         except Exception:
             traceback.print_exc(file=sys.stdout)
@@ -171,9 +158,9 @@ class Workstation(object):
             self.generate_knife_rb(new_path, connection)
             chef_key_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(new_path), 'chefkey.pem'))
             OverthereUtils.write(String(self.client_key).getBytes(), chef_key_file)
-            script="""#!/bin/bash\ncd %s\n%s/bin/knife ssl fetch\n%s/bin/knife ssl check""" % (workspace_path, self.chef_sdk_path, self.chef_sdk_path)
-            script_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(workspace_path), 'ssl.script'))
-            OverthereUtils.write(String(script).getBytes(), script_file)
+            ssl_script=self.get_os_specific_ssl_script(workspace_path)
+            script_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(workspace_path), 'ssl.cmd'))
+            OverthereUtils.write(String(ssl_script).getBytes(), script_file)
             script_file.setExecutable(True)
             self.cmd_line.addArgument(script_file.getPath())
             connection.execute(self.stdout, self.stderr, self.cmd_line)
@@ -181,6 +168,14 @@ class Workstation(object):
         except Exception:
             traceback.print_exc(file=sys.stdout)
             sys.exit(1)
+
+    def get_os_specific_ssl_script(self, workspace_path):
+        if self.unix: return """#!/bin/bash\ncd %s\n%s/bin/knife ssl fetch\n%s/bin/knife ssl check""" % (workspace_path, self.chef_sdk_path, self.chef_sdk_path)
+        else: return """@echo off\r\ncd %s\r\n%s\\bin\\knife.bat ssl fetch\r\n%s\\bin\\knife.bat ssl check\r\n""" % (workspace_path, self.chef_sdk_path, self.chef_sdk_path)
+
+    def get_os_specific_knife_command(self, workspace_path, command, options):
+        if self.unix: return "#!/bin/bash\ncd %s\n%s/bin/knife %s %s" % (workspace_path, self.chef_sdk_path, command, options)
+        else: return "@echo off\r\ncd %s\r\n%s\\bin\\knife.bat %s %s\r\n" % (workspace_path, self.chef_sdk_path, command, options)
 
     def generate_knife_rb(self, path, connection):
         knife_contents ='''current_dir = File.dirname(__FILE__)
@@ -192,3 +187,16 @@ class Workstation(object):
   Cookbook_path %s''' % (self.log_level, self.log_location, self.node_name, self.chef_server_url, self.cookbook_path)
         knife_rb_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(path), 'knife.rb'))
         OverthereUtils.write(knife_contents, knife_rb_file)
+
+    def zip_workspace(self, workspace_path, connection):
+        zip_script = self.get_os_specific_zip_command(workspace_path)
+        zip_script_file = connection.getFile(OverthereUtils.constructPath(connection.getFile(workspace_path), 'zip.cmd'))
+        OverthereUtils.write(String(zip_script).getBytes(), zip_script_file)
+        zip_script_file.setExecutable(True)
+        command = CmdLine()
+        command.addArgument(zip_script_file.getPath())
+        return connection.execute(command)
+
+    def get_os_specific_zip_command(self, workspace_path):
+        if self.unix: return "#!/bin/bash\ncd %s\ntar -czf /tmp/chef.tgz ." % workspace_path
+        else: return "@echo off\r\ncd %s\r\ntar -czf C:\\Windows\\Temp\\chef.tgz .\r\n" % workspace_path
